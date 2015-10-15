02720becf8f6e546eeb1857f697c6e184db9c4e9
stas1k <kuzminoff.stas@gmail.com>
2015-10-15 11:41:00 -0700

    palindrome

--- src/List.hs
+++ src/List.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module List where
 import Prelude
 
@@ -23,3 +24,51 @@ safeSecond xs = if null (tail xs)
 tidySecond:: [a] -> Maybe a
 tidySecond (_:x:_) = Just x
 tidySecond _ = Nothing
+
+count::[a] -> Int
+count [] = 0
+count (_:xs) = 1 + count xs 
+
+summ::[Float] -> Float
+summ [] = 0
+summ (x:xs) =  x + (summ xs)
+
+listMean::[Float] -> Float
+listMean list
+    | length list == 0 = 0.0
+    | otherwise = mean
+    where mean = (summ list) / fromIntegral (count list)
+
+toPalindrome::[a] -> [a]
+toPalindrome list 
+    | length list == 0 = []
+    | otherwise = list ++ reversedList
+   where reversedList = reverse list
+
+reverseList::[a] -> [a]
+reverseList [] = []
+reverseList (x:xs) = reverseList xs ++ [x]
+
+isPalindrome::[a] -> Bool
+isPalindrome list
+    | ((count list) `mod` 2)/= 0 = False
+    | otherwise = result
+   where result = compare' l1 l2
+         l1 = fst $ splitAt ((length list) `div` 2) list
+         l2 = snd $ splitAt ((length list) `div` 2) list
+
+compare':: [c]->[c]->Bool
+compare' a b 
+    |length a /= length b = False
+    |otherwise = result
+   where result = cmpLists a (reverseList b)
+
+cmp::(Eq a)=> a->a->Bool
+cmp x y
+    |x==y = True
+    |otherwise = False
+
+cmpLists:: [a]->[a]->Bool
+cmpLists [] _ = False
+cmpLists _ [] = False
+cmpLists (x:xs) (y:ys) = (cmp x y) && (cmpLists xs ys)
--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,12 @@
 -- | Main entry point to the application.
 module Main where
-import Lending
+import List
 
 -- | The main entry point.
 main :: IO ()
 main  = do
-        print $ lend (100::Int) (500::Int)
-        print $ lend2 (100::Int) (500::Int)
-        print $ lend3 (100::Int) (500::Int)
-       -- let apple1 = "apple"
-        --let orange = "orange"        
-
-        --print $ matchFruits apple1
-
-
-
-data Fruit = Apple | Orange deriving (Show)
-
-
-
-matchFruits:: String -> Fruit
-matchFruits fruit = case fruit of 
-                "apple" -> Apple
-                "orange" -> Orange
+       let a = [1.0..10.0]
+      --print $ listMean a
+       print $ toPalindrome a
+       let b = toPalindrome a
+       print $ isPalindrome b
